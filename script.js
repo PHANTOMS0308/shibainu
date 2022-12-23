f293ee1d0814419a051be883dcc71eaabcbf7223
Shennie <shengfeng.wu.0308@gmail.com>
2022-12-22 22:25:57 -0500

    All materials ready

--- script.js
+++
@@ -1,7 +0,0 @@
-// Simply change CSS checkbox dropdown into JS event driven dropdown
-
-function openNavMenu(event) {
-  this.nextElementSibling.classList.toggle('nav__menu--on');
-}
-
-document.querySelector('#nav__menu__toggler').addEventListener('click', openNavMenu);
